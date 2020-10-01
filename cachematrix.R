1c72fc854b1f2a671903deca6b8a973979e4a0e3
StuartEPoppadom <stuartepoppadom@gmail.com>
2020-10-01 14:19:41 -0500

    Update cachematrix.R
    Completed assignment for Coursera R Programming Week 3 Assignment 2: Lexical Scoping

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,43 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Caching the Inverse of a Matrix
+## A pair of functions that cache the inverse of a matrix instead of
+## computing it repeatedly.
 
-## Write a short comment describing this function
+## Used sample code in 'Example: Caching the Mean of the Vector". Replaced all 
+## code using 'mean' with 'inverse', with one exception. The exception is 
+## that the funtion mean() is replaced with solve() per the instructions 
+## of the assignment.
+
+
+## makeCacheMatrix: creates a special matrix object that can cache its inverse.
 
 makeCacheMatrix <- function(x = matrix()) {
-
+        m <- NULL
+        set <- function(y) {
+                x <<- y
+                m <<- NULL
+        }
+        get <- function() x
+        setinverse <- function(inverse) m <<- inverse
+        getinverse <- function() m 
+        list (set = set, get = get, 
+              setinverse = setinverse,
+              getinverse = getinverse)
 }
 
-
-## Write a short comment describing this function
+       
+## cacheSolve: computes the inverse of the special matrix returned by
+##      makeCacheMatrix above.        
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-}
+        m <- x$getinverse()
+        if(!is.null(m))   {
+                message("getting cached data")
+                return(m)
+        }
+        data <- x$get()
+        m <- solve(data, ...)
+        x$setinverse(m)
+        m
+}     
+
+        
